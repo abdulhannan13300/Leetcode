08324544fb02a7a0b9fa09d66cf16537100d9e3a
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-03-22 22:06:48 +0530

    Add files via upload

---
+++ 13.Roman_to_Integer.java
@@ -0,0 +1,118 @@
+13. Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+ 
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+***********************************************************************************
+class Solution {
+    public int romanToInt(String s) {
+        
+        int num = 0; 
+        int ans = 0;
+        int prev = 0;
+
+        //Start for the last char and we will move backward
+        for(int i = s.length()-1; i >= 0; i--){
+            switch(s.charAt(i)) {
+                case 'I' : num = 1; break;
+                case 'V' : num = 5; break;
+                case 'X' : num = 10; break;
+                case 'L' : num = 50; break;
+                case 'C' : num = 100; break;
+                case 'D' : num = 500; break;
+                case 'M' : num = 1000; break;
+            }
+            
+            //If the current number is less then the prev number then we will to subtract as the small number have arrived before the larger number.
+            if(num < prev) 
+                ans -= num; 
+            else 
+                ans += num;
+        //we will keep track of the prev number to compare.
+        prev = num;
+        }
+    return ans;    
+    }
+//Step wise example explained for better understanding
+/* 
+eg : CMXCVIII = 998
+
+I
+num = 1;
+prev = 0;
+ans = 0 + 1 = 1;
+prev = 1;
+
+I
+num = 1;
+prev = 1;
+ans = 1 + 1 = 2;
+prev = 1;
+
+I
+num = 1;
+prev = 1;
+ans = 2 + 1 = 3;
+prev = 1;
+
+V
+num = 5;
+prev = 1;
+ans = 3 + 5 = 8;
+prev = 5;
+
+C
+num = 100;
+prev = 5;
+ans = 8 + 100 = 108;
+prev = 100;
+
+
+X
+num = 10;
+prev = 100;
+num < prev
+ans = 108 - 10 = 98;
+prev = 10;
+
+M
+num = 1000;
+prev = 10;
+ans = 98+1000= 1098
+prev = 1000;
+
+C
+num = 100;
+prev = 1000;
+num < prev
+ans = 1098 - 100 = 998 -->
+prev = 100;
+*/
+}
+**********************************************************************************
