458f6477df43bbe208e33222d57c0950c812ee6c
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-03-29 17:06:56 +0530

    Add files via upload

---
+++ 28.Find_the_Index_of_the_First_Occurrence_in_String.cpp
@@ -0,0 +1,35 @@
+28. Find the Index of the First Occurrence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+************************************************************************
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.size()<needle.size())
+            return -1;
+        
+        for(int i=0; i<=haystack.size()-needle.size() ; i++){
+            int j=0;
+            if(haystack.at(i)==needle.at(j)){
+                while(j<needle.size() && haystack.at(i+j) == needle.at(j)){
+                    j++;
+                }
+                if(j==needle.size())
+                    return i;
+            }
+        }
+        return -1;
+    }
+};
+************************************************************************
